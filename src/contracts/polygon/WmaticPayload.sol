ef59f701843b1a2879bbb9f6385d599b64925335
defijesus.eth <defijesus@buzzedbearhideout.com>
2022-11-02 00:23:49 +0000

    ready for review

--- src/contracts/polygon/WmaticPayload.sol
+++ src/contracts/polygon/WmaticPayload.sol
@@ -2,25 +2,12 @@
 pragma solidity ^0.8.0;
 
 import {AaveV3Polygon} from 'aave-address-book/AaveV3Polygon.sol';
-import {IPoolConfigurator} from 'aave-address-book/AaveV3.sol';
 import {IProposalGenericExecutor} from '../../interfaces/IProposalGenericExecutor.sol';
 
 /**
- * @dev This payload lists FRAX as collateral and borrowing asset on Aave V3 Polygon
- * - Parameter snapshot: https://snapshot.org/#/aave.eth/proposal/0xa464894c571fecf559fab1f1a8daf514250955d5ed2bc21eb3a153d03bbe67db
- * Opposed to the suggested parameters this proposal will
- * - Lowering the suggested 50M ceiling to a 2M ceiling
- * - Adding a 50M supply cap
- * - The eMode lq treshold will be 97.5, instead of the suggested 98% as the parameters are per emode not per asset
- * - The reserve factor will be 10% instead of 5% to be consistent with other stable coins
+ * @dev This payload changes the interest rate strategy of WMATIC to a new one
  */
 contract WmaticPayload is IProposalGenericExecutor {
-  // **************************
-  // Protocol's contracts
-  // **************************
-  address public constant INCENTIVES_CONTROLLER =
-    0x929EC64c34a17401F460460D4B9390518E5B473e;
-
   // **************************
   // Interest Rate Params
   // **************************
--- src/test/polygon/PolygonWmaticE2E.t.sol
+++ src/test/polygon/PolygonWmaticE2E.t.sol
@@ -28,11 +28,17 @@ contract PolygonWmaticE2ETest is ProtocolV3TestBase {
   address public constant POLYGON_BRIDGE_EXECUTOR =
     0xdc9A35B16DB4e126cFeDC41322b3a36454B1F772;
 
-  address public constant WMATIC = 0x0d500B1d8E8eF31E21C99d1Db9A6444d3ADf1270;
-
   address public constant INTEREST_RATE_STRATEGY = 0xb9b42D95Be3350899706ca7C9EA3aAcb226C504F;
 
   IInterestRateStrategy public constant OLD_INTEREST_RATE_STRATEGY = IInterestRateStrategy(0x03733F4E008d36f2e37F0080fF1c8DF756622E6F);
+
+  // this ain't read from the old interest rate contract directly because it's a internal variable.
+  uint256 public constant OLD_BASE_STABLE_RATE_OFFSET = 20000000000000000000000000;
+  
+  uint256 public constant NEW_VARIABLE_SLOPE_1 = 61000000000000000000000000;
+  uint256 public constant NEW_VARIABLE_SLOPE_2 = 1000000000000000000000000000;
+  uint256 public constant NEW_OPTIMAL_USAGE_RATIO = 750000000000000000000000000;
+  uint256 public constant NEW_BASE_STABLE_BORROW_RATE = NEW_VARIABLE_SLOPE_1 + OLD_BASE_STABLE_RATE_OFFSET;
 
   function setUp() public {
     polygonFork = vm.createFork(vm.rpcUrl('polygon'), 34808266);
@@ -84,141 +90,24 @@ contract PolygonWmaticE2ETest is ProtocolV3TestBase {
     BridgeExecutorHelpers.waitAndExecuteLatest(vm, POLYGON_BRIDGE_EXECUTOR);
 
     // 6. verify results
-    ReserveConfig[] memory allConfigsAfter = _getReservesConfigs(
-      AaveV3Polygon.POOL
+
+    InterestStrategyValues memory expectedIntRateStrat = InterestStrategyValues({
+      addressesProvider: address(OLD_INTEREST_RATE_STRATEGY.ADDRESSES_PROVIDER()),
+      optimalUsageRatio: NEW_OPTIMAL_USAGE_RATIO,
+      optimalStableToTotalDebtRatio: OLD_INTEREST_RATE_STRATEGY.OPTIMAL_STABLE_TO_TOTAL_DEBT_RATIO(),
+      baseStableBorrowRate: NEW_BASE_STABLE_BORROW_RATE,
+      stableRateSlope1: OLD_INTEREST_RATE_STRATEGY.getStableRateSlope1(),
+      stableRateSlope2: OLD_INTEREST_RATE_STRATEGY.getStableRateSlope2(),
+      baseVariableBorrowRate: OLD_INTEREST_RATE_STRATEGY.getBaseVariableBorrowRate(),
+      variableRateSlope1: NEW_VARIABLE_SLOPE_1,
+      variableRateSlope2: NEW_VARIABLE_SLOPE_2
+    });
+
+    _validateInterestRateStrategy(
+      payload.INTEREST_RATE_STRATEGY(),
+      INTEREST_RATE_STRATEGY,
+      expectedIntRateStrat
     );
-
-    // TODO Validation
-
-    // InterestStrategyValues memory expectedIntRateStrat = InterestStrategyValues({
-    //   addressesProvider: 0xa97684ead0e402dC232d5A977953DF7ECBaB3CDb,
-    //   optimalUsageRatio: 750000000000000000000000000,
-    //   baseVariableBorrowRate: OLD_INTEREST_RATE_STRATEGY.getBaseVariableBorrowRate(),
-    //   variableRateSlope1: 61000000000000000000000000,
-    //   variableRateSlope2: 1000000000000000000000000000,
-    //   stableRateSlope1: OLD_INTEREST_RATE_STRATEGY.getStableRateSlope1(),
-    //   stableRateSlope2: OLD_INTEREST_RATE_STRATEGY.getStableRateSlope2(),
-    //   baseStableRateOffset: 20000000000000000000000000,
-    //   stableRateExcessOffset: OLD_INTEREST_RATE_STRATEGY.getStableRateExcessOffset(),
-    //   optimalStableToTotalDebtRatio: OLD_INTEREST_RATE_STRATEGY.OPTIMAL_STABLE_TO_TOTAL_DEBT_RATIO()
-    // });
-
-    // _validateInterestRateStrategy(
-    //   payload.INTEREST_RATE_STRATEGY(),
-    //   0xb9b42D95Be3350899706ca7C9EA3aAcb226C504F,
-    //   expectedIntRateStrat
-    // );
-
-    // ReserveConfig memory expectedAssetConfig = ReserveConfig({
-    //   symbol: 'FRAX',
-    //   underlying: FRAX,
-    //   aToken: address(0), // Mock, as they don't get validated, because of the "dynamic" deployment on proposal execution
-    //   variableDebtToken: address(0), // Mock, as they don't get validated, because of the "dynamic" deployment on proposal execution
-    //   stableDebtToken: address(0), // Mock, as they don't get validated, because of the "dynamic" deployment on proposal execution
-    //   decimals: 18,
-    //   ltv: 7500,
-    //   liquidationThreshold: 8000,
-    //   liquidationBonus: 10500,
-    //   liquidationProtocolFee: 1000,
-    //   reserveFactor: 1000,
-    //   usageAsCollateralEnabled: true,
-    //   borrowingEnabled: true,
-    //   interestRateStrategy: _findReserveConfigBySymbol(allConfigsAfter, 'USDT')
-    //     .interestRateStrategy,
-    //   stableBorrowRateEnabled: false,
-    //   isActive: true,
-    //   isFrozen: false,
-    //   isSiloed: false,
-    //   supplyCap: 50_000_000,
-    //   borrowCap: 0,
-    //   debtCeiling: 2_000_000_00,
-    //   eModeCategory: 1
-    // });
-
-    // _validateReserveConfig(expectedAssetConfig, allConfigsAfter);
-
-    // _noReservesConfigsChangesApartNewListings(
-    //   allConfigsBefore,
-    //   allConfigsAfter
-    // );
-
-    // _validateReserveTokensImpls(
-    //   AaveV3Polygon.POOL_ADDRESSES_PROVIDER,
-    //   _findReserveConfigBySymbol(allConfigsAfter, 'FRAX'),
-    //   ReserveTokens({
-    //     aToken: fraxPayload.ATOKEN_IMPL(),
-    //     stableDebtToken: fraxPayload.SDTOKEN_IMPL(),
-    //     variableDebtToken: fraxPayload.VDTOKEN_IMPL()
-    //   })
-    // );
-
-    // this._validateAssetSourceOnOracle(
-    //   AaveV3Polygon.POOL_ADDRESSES_PROVIDER,
-    //   FRAX,
-    //   fraxPayload.PRICE_FEED()
-    // );
-
-    // // impl should be same as USDC
-    // _validateReserveTokensImpls(
-    //   AaveV3Polygon.POOL_ADDRESSES_PROVIDER,
-    //   _findReserveConfigBySymbol(allConfigsAfter, 'USDC'),
-    //   ReserveTokens({
-    //     aToken: fraxPayload.ATOKEN_IMPL(),
-    //     stableDebtToken: fraxPayload.SDTOKEN_IMPL(),
-    //     variableDebtToken: fraxPayload.VDTOKEN_IMPL()
-    //   })
-    // );
-
-    // _validatePoolActionsPostListing(allConfigsAfter);
-  }
-
-  function _validatePoolActionsPostListing(
-    ReserveConfig[] memory allReservesConfigs
-  ) internal {
-    ReserveConfig memory frax = _findReserveConfigBySymbol(
-      allReservesConfigs,
-      'FRAX'
-    );
-    ReserveConfig memory dai = _findReserveConfigBySymbol(
-      allReservesConfigs,
-      'DAI'
-    );
-
-    address user0 = address(1);
-    _deposit(frax, AaveV3Polygon.POOL, user0, 666 ether);
-
-    this._borrow(dai, AaveV3Polygon.POOL, user0, 2 ether, false);
-
-    // We check revert when trying to borrow (not enabled in isolation)
-    try this._borrow(frax, AaveV3Polygon.POOL, user0, 300 ether, false) {
-      revert('_testProposal() : BORROW_NOT_REVERTING');
-    } catch Error(string memory revertReason) {
-      require(
-        keccak256(bytes(revertReason)) == keccak256(bytes('60')),
-        '_testProposal() : INVALID_VARIABLE_REVERT_MSG'
-      );
-      vm.stopPrank();
-    }
-
-    // We check revert when trying to borrow (not enabled in isolation)
-    try this._borrow(frax, AaveV3Polygon.POOL, user0, 10 ether, true) {
-      revert('_testProposal() : BORROW_NOT_REVERTING');
-    } catch Error(string memory revertReason) {
-      require(
-        keccak256(bytes(revertReason)) == keccak256(bytes('60')),
-        '_testProposal() : INVALID_STABLE_REVERT_MSG'
-      );
-      vm.stopPrank();
-    }
-
-    this._borrow(dai, AaveV3Polygon.POOL, user0, 222 ether, false);
-
-    // Not possible to borrow and repay when vdebt index doesn't changing, so moving 1s
-    skip(1);
-
-    _repay(dai, AaveV3Polygon.POOL, user0, 1000 ether, false);
-
-    _withdraw(frax, AaveV3Polygon.POOL, user0, type(uint256).max);
   }
 
   // utility to transform memory to calldata so array range access is available
